2bcf5383a7cbca7b3a8a23939abfbb624487a404
Jonas Andersen <Jonas@anders1.com>
2023-10-21 01:27:41 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import "./App.css";
 import MyCheckbox from "./components/Checkbox";
 import MySlider from "./components/slider";
 
+enum PasswordStrength {
+  "STRONG" = 4,
+  "MEDIUM" = 3,
+  "NORMAL" = 2,
+  "WEAK" = 1
+}
 
 function App() {
   const [strength, setStrength] = useState(0)
@@ -11,14 +17,13 @@ function App() {
   const [includeLowercase, setIncludeLowercase] = useState(false)
   const [includeNumbers, setIncludeNumbers] = useState(false)
   const [includeSymbols, setIncludeSymbols] = useState(false)
-
-  let strengthArray: boolean[] = []
+  const [strengthArray, setStrengthArray] = useState([false, false, false, false])
+  
   let strengthValue = 0
   useEffect(() => {
     // an array of all the booleans
-    strengthArray = [includeLowercase, includeUppercase, includeNumbers, includeSymbols]
+    setStrengthArray([includeLowercase, includeUppercase, includeNumbers, includeSymbols])
     // console.log(strengthArray)
-    
     // keeps track of how many of the checkboxes that is checked
     strengthValue = strengthArray.filter(value =>  value === true).length
     setStrength(strengthValue)
@@ -67,7 +72,15 @@ function App() {
           >
             <div className="strength-container">
               <p>Strength</p>
-              {strength}
+              <p>{strength == 0 ? "WEAK" : strength == 1 ? "WEAK" : strength == 2 ? "MEDIUM" : strength == 3 ? "GOOD" : strength == 4 ? "STRONG" : ""}</p>
+              <div style={{display:"flex"}}>
+
+              {/* Sort the array to show the "true" occurrences fist then loop over the array */}
+              {strengthArray.sort((a, b) => (a === b ? 0 : a ? -1 : 1)).map((value, index) => (
+                <h1 key={index} style={ value == true ? {color: "#a5ffaf"} : {color:"gray"}}>#</h1>
+              ))}
+
+              </div>
             </div>
             <div>
               <button className="button">Generate</button>
